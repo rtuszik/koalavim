9cd1c0999a0328ddc174423d13b555da38f9ee5c
Robin Tuszik <mail@robin.gg>
2025-12-07 13:13:33 +0100

    remove fzf lsp keybindings

--- lua/plugins/editor/fzf.lua
+++ lua/plugins/editor/fzf.lua
@@ -239,24 +239,4 @@ return {
         },
     },
 
-    {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                ["*"] = {
-                    keys = {
-                        {
-                            "gd",
-                            "<cmd>FzfLua lsp_definitions jump1=true ignore_current_line=true<cr>",
-                            desc = "Goto Definition",
-                            has = "definition",
-                        },
-                        { "gr", "<cmd>FzfLua lsp_references jump1=true ignore_current_line=true<cr>", desc = "References", nowait = true },
-                        { "gI", "<cmd>FzfLua lsp_implementations jump1=true ignore_current_line=true<cr>", desc = "Goto Implementation" },
-                        { "gy", "<cmd>FzfLua lsp_typedefs jump1=true ignore_current_line=true<cr>", desc = "Goto T[y]pe Definition" },
-                    },
-                },
-            },
-        },
-    },
 }
