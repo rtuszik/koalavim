8e316aafee4480122cacfd176ac64131868aa7db
Robin Tuszik <mail@robin.gg>
2025-12-07 12:55:15 +0100

    add ts-comments and mini.ai plugins

--- lua/plugins/editor/code.lua
+++ lua/plugins/editor/code.lua
@@ -19,7 +19,11 @@ return {
                 desc = "Previous Todo Comment",
             },
             { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
-            { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+            {
+                "<leader>xT",
+                "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
+                desc = "Todo/Fix/Fixme (Trouble)",
+            },
         },
     },
     {
@@ -30,5 +34,44 @@ return {
     {
         "nmac427/guess-indent.nvim",
         lazy = false,
+    },
+    {
+        "folke/ts-comments.nvim",
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
+        "nvim-mini/mini.ai",
+        event = "VeryLazy",
+        opts = function()
+            local ai = require "mini.ai"
+            return {
+                n_lines = 500,
+                custom_textobjects = {
+                    o = ai.gen_spec.treesitter { -- code block
+                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                    },
+                    f = ai.gen_spec.treesitter { a = "@function.outer", i = "@function.inner" }, -- function
+                    c = ai.gen_spec.treesitter { a = "@class.outer", i = "@class.inner" }, -- class
+                    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+                    d = { "%f[%d]%d+" }, -- digits
+                    e = { -- Word with case
+                        {
+                            "%u[%l%d]+%f[^%l%d]",
+                            "%f[%S][%l%d]+%f[^%l%d]",
+                            "%f[%P][%l%d]+%f[^%l%d]",
+                            "^[%l%d]+%f[^%l%d]",
+                        },
+                        "^().*()$",
+                    },
+                    u = ai.gen_spec.function_call(), -- u for "Usage"
+                    U = ai.gen_spec.function_call { name_pattern = "[%w_]" }, -- without dot in function name
+                },
+            }
+        end,
+        config = function(_, opts)
+            require("mini.ai").setup(opts)
+        end,
     },
 }
