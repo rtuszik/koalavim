18b38e345140ea3ea203a40b3c7eeed50a79ac8c
Robin Tuszik <mail@robin.gg>
2026-03-30 17:32:19 +0200

    style(git): update gitsigns signs and add staged signs config

--- lua/plugins/tools/git.lua
+++ lua/plugins/tools/git.lua
@@ -19,12 +19,30 @@ return {
         "lewis6991/gitsigns.nvim",
         opts = {
             signs = {
-                add = { text = "+" },
-                change = { text = "~" },
+                add = { text = "┃" },
+                change = { text = "┃" },
                 delete = { text = "_" },
                 topdelete = { text = "‾" },
                 changedelete = { text = "~" },
+                untracked = { text = "┆" },
             },
+            signs_staged = {
+                add = { text = "┃" },
+                change = { text = "┃" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "┆" },
+            },
+            signs_staged_enable = true,
+            signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+            numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+            linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+            word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+            watch_gitdir = {
+                follow_files = true,
+            },
+            current_line_blame = true,
         },
     },
     {
