8b3078a377e31cb1f831b4ac5a2983976381473d
Robin Tuszik <mail@robin.gg>
2026-08-19 19:29:10 +0200

    refactor(treesitter): migrate to the main branch API

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,44 +1,66 @@
+local parsers = {
+    "bash",
+    "c",
+    "comment",
+    "css",
+    "csv",
+    "diff",
+    "dockerfile",
+    "editorconfig",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "go",
+    "gotmpl",
+    "hcl",
+    "helm",
+    "html",
+    "javascript",
+    "json",
+    "json5",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "mermaid",
+    "php",
+    "python",
+    "query",
+    "regex",
+    "ruby",
+    "rust",
+    "sql",
+    "ssh_config",
+    "swift",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "yaml",
+    "zig",
+    "zsh",
+}
+
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
-        main = "nvim-treesitter", -- lazy.nvim will call this module's setup()
-        opts = {
-            ensure_installed = {
-                "bash",
-                "c",
-                "css",
-                "dockerfile",
-                "git_config",
-                "gitcommit",
-                "gitignore",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
-                },
-            },
-        },
+        config = function()
+            require("nvim-treesitter").install(parsers)
+
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("treesitter-features", {}),
+                callback = function(ev)
+                    if pcall(vim.treesitter.start, ev.buf) then
+                        vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
+            })
+        end,
     },
     {
         "windwp/nvim-ts-autotag",
