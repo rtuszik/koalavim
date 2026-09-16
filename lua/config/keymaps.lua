9cd7e16e8d298ffc65cd5e80315564b23eeebaae
Robin Tuszik <mail@robin.gg>
2026-09-16 19:38:17 +0200

    fix(keymaps): use checkhealth for LSP info shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -235,7 +235,7 @@ function M.get()
   end
   -- stylua: ignore
   M._keys = {
-    { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+    { "<leader>cl", "<cmd>checkhealth vim.lsp<cr>", desc = "LSP Info" },
     { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
     { "gr", vim.lsp.buf.references, desc = "References", nowait = true },
     { "gI", vim.lsp.buf.implementation, desc = "Goto Implementation" },
